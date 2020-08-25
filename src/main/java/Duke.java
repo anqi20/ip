2da858133764001c4b508b1b5cdb00013df661c0
anqi20 <anqi20@hotmail.com>
2020-08-26 00:50:40 +0800

    Level-1: Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String userCommand;
+        boolean isExit = false;
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -11,7 +17,32 @@ public class Duke {
         System.out.println("--------------------------------------------------");
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
         System.out.println("--------------------------------------------------");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("--------------------------------------------------");
+
+        //Level 1: Greet, Echo, Exit
+        while(!isExit) {
+            userCommand = in.nextLine();
+
+            if (userCommand.equals("list")) {
+                System.out.println("--------------------------------------------------");
+                System.out.println("Here is your list!");
+                System.out.println("--------------------------------------------------");
+            } else if (userCommand.equals("blah")) {
+                System.out.println("--------------------------------------------------");
+                System.out.println("Blah blah blah blah blah");
+                System.out.println("Hahaha, kidding! What were you saying again?");
+                System.out.println("--------------------------------------------------");
+            } else if (userCommand.equals("bye")) {
+                System.out.println("--------------------------------------------------");
+                System.out.println("Bye. Hope you have a nice day and see you soon!");
+                System.out.println("--------------------------------------------------");
+                isExit = true;
+            } else {
+                System.out.println("--------------------------------------------------");
+                System.out.println("Errr... I don't really understand what you are saying.");
+                System.out.println("Here is the list of commands that I can understand: ");
+                System.out.println("1) list\n2) blah\n3) bye");
+                System.out.println("--------------------------------------------------");
+            }
+        }
     }
 }
