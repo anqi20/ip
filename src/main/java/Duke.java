69a7b683fd042176e2b660197557019599252046
anqi20 <anqi20@hotmail.com>
2020-09-09 20:44:19 +0800

    Revert "Improved on the code quality by creating class "Replies""
    This reverts commit a89164bf0fd6c9f805a0fe7d53f2a47d256f1fbb.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -27,6 +27,37 @@ public class Duke {
         return d;
     }
 
+    public static void printEntireList(Task[] list) {
+        int i = 0;
+        for(Task item: list) {
+            if(item != null) {
+                System.out.println((i+1) + ". "+ item.toString());
+                i++;
+            }
+        }
+    }
+
+    public static void replyToAddTask(int counterList, String DASH_LINE, String s) {
+        System.out.println(DASH_LINE);
+        System.out.println("Got it! I've added this task: ");
+        System.out.println(s);
+        System.out.print("Now you have " + counterList + " task");
+        if (counterList == 1) {
+            System.out.print(" ");
+        } else {
+            System.out.print("s ");
+        }
+        System.out.println("in the list.");
+        System.out.println(DASH_LINE);
+    }
+
+    public static void replyToList(Task[] entireList, String DASH_LINE) {
+        System.out.println(DASH_LINE);
+        System.out.println("Here is your list!");
+        printEntireList(entireList);
+        System.out.println(DASH_LINE);
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String userInput;
@@ -34,6 +65,7 @@ public class Duke {
         int counterList = 0;
         boolean isExit = false;
         boolean isInvalid = false;
+        boolean isUnsure = false;
 
         //Constants
         final int LENGTH_OF_BY = 3; // or LENGTH_OF_AT
@@ -41,9 +73,18 @@ public class Duke {
         final int LENGTH_OF_TODO = 4;
         final int LENGTH_OF_EVENT = 5;
         final int LENGTH_OF_DEADLINE = 8;
+        final String DASH_LINE = "--------------------------------------------------";
+        String LOGO = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + LOGO);
 
         //Greetings
-        Replies.printGreetings();
+        System.out.println(DASH_LINE);
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+        System.out.println(DASH_LINE);
 
         while(!isExit) {
             userInput = in.nextLine();
@@ -51,35 +92,49 @@ public class Duke {
 
             if (userCommand.equals("list")) {
                 if(counterList > 0) {
-                    Replies.printList(entireList);
+                    replyToList(entireList, DASH_LINE);
                 }
 
                 else if(counterList == 0) {
-                    Replies.printEmptyList();
+                    System.out.println(DASH_LINE);
+                    System.out.println("There is nothing on your list! Add something to begin! ");
+                    System.out.println(DASH_LINE);
                 }
             }
 
             else if (userCommand.equals("blah")) {
-                Replies.printBlah();
+                System.out.println(DASH_LINE);
+                System.out.println("Blah blah blah blah blah");
+                System.out.println("Hahaha, kidding! What were you saying again?");
+                System.out.println(DASH_LINE);
             }
 
             else if (userCommand.equals("bye")) {
-                Replies.printBye();
+                System.out.println(DASH_LINE);
+                System.out.println("Bye bye. Hope you have a nice day and see you soon!");
+                System.out.println(DASH_LINE);
                 isExit = true;
             }
 
             else if (userCommand.startsWith("done")) {
                 String taskNumString = userCommand.substring(LENGTH_OF_DONE);
-                int taskNum = Integer.parseInt(taskNumString.trim()) - 1;
+                int taskNumInt = Integer.parseInt(taskNumString.trim()) - 1;
 
                 //Valid "done" command
-                if((taskNum < counterList) && (taskNum >= 0)) {
-                    entireList[taskNum].markAsDone();
-                    Replies.printDoneValid(entireList, taskNum);
+                if((taskNumInt < counterList) && (taskNumInt >= 0)) {
+                    entireList[taskNumInt].markAsDone();
+                    System.out.println(DASH_LINE);
+                    System.out.println("Congratulations! You have completed: ");
+                    System.out.println((taskNumInt + 1) + ". " + entireList[taskNumInt].toString());
+                    System.out.println(DASH_LINE);
                 }
                 //Invalid "done" command
                 else {
-                    Replies.printDoneInvalid(entireList);
+                    System.out.println(DASH_LINE);
+                    System.out.println("Errr... I don't think you have this item on the list.");
+                    System.out.println("This is the current list that you have:");
+                    printEntireList(entireList);
+                    System.out.println(DASH_LINE);
                 }
             }
 
@@ -91,7 +146,7 @@ public class Duke {
                     entireList[counterList] = d;
                     counterList++;
 
-                    Replies.printToAddTask(d.toString(), counterList);
+                    replyToAddTask(counterList, DASH_LINE, d.toString());
                 }
 
                 //Invalid "deadline" command
@@ -108,7 +163,7 @@ public class Duke {
                     entireList[counterList] = e;
                     counterList++;
 
-                    Replies.printToAddTask(e.toString(), counterList);
+                    replyToAddTask(counterList, DASH_LINE, e.toString());
                 }
 
                 //Invalid "Events" command
@@ -122,19 +177,34 @@ public class Duke {
                 entireList[counterList] = t;
                 counterList++;
 
-                Replies.printToAddTask(t.toString(), counterList);
+                replyToAddTask(counterList, DASH_LINE, t.toString());
             }
 
             else if(userCommand.equals("?")) {
-                Replies.printUnsure();
+                isUnsure = true;
             }
 
             else {
-                Replies.printUnsure();
+                isUnsure = true;
+            }
+
+            if(isUnsure) {
+                System.out.println(DASH_LINE);
+                System.out.println("You must be confused! \u2719_\u2719");
+                System.out.println("Here is the list of commands that I can understand: ");
+                System.out.println("1) list\n2) blah\n3) todo...\n4) event... /at...\n5) deadline... /by...\n6) ?\n7)" +
+                        " bye");
+                System.out.println(DASH_LINE);
+                isUnsure = false;
             }
 
             if(isInvalid) {
-                Replies.printInvalid();
+                System.out.println(DASH_LINE);
+                System.out.println("Invalid command!");
+                System.out.println("This is the formatting for the commands: ");
+                System.out.println("    deadline <description> /by <date>");
+                System.out.println("    event <description> /at <date>");
+                System.out.println(DASH_LINE);
                 isInvalid = false;
             }
         }
